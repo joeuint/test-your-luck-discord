25786e86b7945c854438db1f2fd44564941fce4a
joqwer <94891082+joqwer@users.noreply.github.com>
2022-08-31 22:42:54 -0400

    Finish the shell script (deploy.sh)

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
+# cd into the basedir
 BASEDIR=$(dirname "$0")
-
 cd $BASEDIR
 
+# Confirm
+read -p "Hello, this script pulls from git and redeploys the bot using pm2. Would you like to continue? (y/N) " -n 1 -r
+
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    # The user agreed
+    git pull
+    pm2 restart discord-test-your-luck
+fi
